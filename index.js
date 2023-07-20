f86b0001a49400b38a5a3852b833350290ec5c38
Sabina <justkrupasabina@gmail.com>
2023-07-20 22:27:21 +0300

    Add join

--- index.js
+++ index.js
@@ -1,26 +1,75 @@
-//Написати функцію checkValueInArray, яка приймає масив чисел і ключ(число), і повертає булеве значеня залежно від того чи присутній в масиві ключ.
-//Написати функцію, яка приймає масив з числами і повертає true, якщо в масиві є два однакових числа поспіль, інакше повертає false. Якщо масив порожній також повертає false.
-//викотристовувати методи масивів
+//мутация данных
+// const arr1 = new Array(7, 4, 5);
+// arr1[0] = 5555;
+// arr1[0] = null;
+// arr1[6] = 55;
 
-/**
- * @param {Array<number>} array1
- * @param {number} key1
- */
+// const arr2 = new Array(73, 44, 55);
 
-const array1 = [11, 22, 33, 44, 55, 55];
-const key1 = 33;
+// let arrCurrent = Math.random()>0.5 ? arr1 : arr2;
+// arrCurrent[1] = 1000;
+// console.log(arrCurrent.indexOf(5));
 
-function checkValueInArray(array, key){
-    return array.includes(key);
-}
-console.log(checkValueInArray(array1, key1));
+// if(Math.random()>0.5){
+//     console.log(arr1.indexOf(5));
+// }else{
+//     console.log(arr2.indexOf(5))
+// }
 
-function checkIdenticalValueInArray(array){
-    for(let i = 0; i < array.length; i++){
-            if(array[i] === array[i+1]){
-                return true;
-            }
-    }
-    return false;
-}
-console.log(checkIdenticalValueInArray(array1, key1));
+//slice
+// const arr1 = [1,2,3,4,5,6,7,8,9];
+
+// const sliceArr1 = arr1.slice(); //slice возвращает поверхностную копию массива
+// const sliceArr2 = arr1.slice(2); //срезали часть с 0 до 2
+// const sliceArr3 = arr1.slice(2, 7); //срезали с 2 до 7
+
+// console.log(arr1); //[1,2,3,4,5,6,7,8,9];
+// console.log(sliceArr1); //[1,2,3,4,5,6,7,8,9];
+// console.log(sliceArr2);//[3,4,5,6,7,8,9];
+
+//splice
+// const arr2 = [1,2,3,4,5,6,7,8,9];
+
+// const start = 3;
+// const deleteCount = 2;
+
+// // const spliceArr1 = arr2.splice(start); //разделяет массив начиная с 3 тк start=3
+// // const spliceArr1 = arr2.splice(start, deleteCount); //удалили 2 и 3 индекс
+// const spliceArr1 = arr2.splice(start, deleteCount, '+') // удалили 2 и 3 индекс и вставили туда +
+
+// console.log(arr2); 
+// console.log(spliceArr1); 
+
+//сделать так чтобы было:
+//Task1 1,2,3  
+//Task2  1,4,5   
+//Task3  1, 'a', 'b, 5
+
+const arr1 = [1,2,3,4,5];
+
+//Task 1 
+// const start = 3;
+// const spliceArr1 = arr1.splice(start);
+// console.log(arr1); //1, 2, 3
+// console.log(spliceArr1); 
+
+//Task 2
+// const start = 1;
+// const deleteCount = 2;
+// const spliceArr1 = arr1.splice(start, deleteCount);
+// console.log(arr1); //1, 4, 5
+// console.log(spliceArr1); //2, 3
+
+//Task 3
+// const start = 1;
+// const deleteCount = 3;
+// const spliceArr1 = arr1.splice(start, deleteCount, 'a', 'b')
+// console.log(arr1); //1, 'a', 'b', 5
+// console.log(spliceArr1); //2, 3, 4
+
+//join
+const arr2 = [1,2,3,4,5];
+
+const strArray1 = arr2.join(':'); //переобразовываем массив 
+
+console.log(strArray1) //1:2:3:4:5
