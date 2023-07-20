f91838594caaed41137c7862093e2b4e2aba46bb
Sabina <justkrupasabina@gmail.com>
2023-07-20 10:43:30 +0300

    Add arguments

--- MyArray.js
+++ MyArray.js
@@ -1,35 +1,79 @@
 // //функция конструтор для прототипа
 // логический функционал
 function PrototypeMyArray(){
-    this.push = function(value){
-        this[this.length] = value;
+    this.push = function(){
+        for (let index = 0; index < arguments.length; index++) {
+            this[this.length] = arguments[index];
         this.length++;
-        return this.length;
-    };
+    }
+    return this.length;
+}
     this.toString = function(){
         let stringArray = '';
         for (let index = 0; index < this.length; index++){
             if(index === this.length - 1){ //чтобы в последнем не ставилась ','
                  stringArray += this[index];
-            } else {
+                 break;
+            }   
                  stringArray += this[index] + ',';
-            }
+            
         }
         return stringArray;
     }
+
+    this.pop = function(){ //прописываем метод pop
+        if(this.length === 0){
+            return; //возвращает undefined если массив this пустой
+        }
+        const lastElem = this[this.length-1]; 
+        delete this[this.length-1];
+        return this[this.length-1];
+        this.length--;
+        return lastElem; //сохраняет значение последнего элемента массива, потом удаляет его и изменяет длину 
+        
+    }
+
 }
 
 // данные
-function MyArray(){
-    this.length = 0;
-}
+// function MyArray(){
+//     this.length = 0;
+// }
 
 //связываем данные с логическим функционалом
 MyArray.prototype = new PrototypeMyArray();
 
-const myArr1 = new MyArray();
-myArr1.push(12);
-myArr1.push(34);
-myArr1.push(23);
+// const myArr1 = new MyArray();
+// myArr1.push(12);
+// myArr1.push(34);
+// myArr1.push(23);
 // console.log(myArr1);
 // console.log(myArr1.toString());
+
+
+//аргументы функции
+const arr1 = new Array(7, 4, 5);
+function f(num1, num2) {
+    console.log('num1 = ', num1); //100
+    console.log('num2 = ', num2); //5
+    console.log(arguments);
+}
+f(100, 5, 6, 8);
+
+//нужно положить аргументы в функцию
+function MyArray(){
+        this.length = 0;
+        if(arguments.length ===1){
+            for (let index = 0; index < arguments[0]; index++){
+                this.push(); //если значение только 1 как в const myArr2, то будет undefined
+            }
+        } else{
+            for (let index = 0; index < arguments.length; index++) {
+                this.push(arguments[index]); //метод push уже описан выше, если больше 1 значения
+            }
+        }
+        
+    }
+
+const myArr1 = new MyArray(4, 8, 6);
+const myArr2 = new MyArray(3);
